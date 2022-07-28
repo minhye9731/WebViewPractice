83ef17a0a8d8be16e414190d586b21ca34859988
minhye9731 <jenney62639731@gmail.com>
2022-07-28 19:17:24 +0900

    Initial Commit

---
+++ WebViewPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebViewPractice
+//
+//  Created by 강민혜 on 7/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
